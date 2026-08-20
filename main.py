6867e1846d3fd154010dbc3babdf0aa49c085b3d
Manraj Singh <162640559+manrajs1@users.noreply.github.com>
2026-08-19 20:40:50 -0700

    Added basic FastAPI routes

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/monitors")
+def num_monitors():
+    return {"message": "There are 5 monitors"}
+
+@app.get("/status")
+def status():
+    return {"message" : "Status is good"}
