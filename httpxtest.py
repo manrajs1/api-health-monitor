97ba33c00984b654e2ea60bacf1794215c42c1de
Manraj Singh <162640559+manrajs1@users.noreply.github.com>
2026-08-21 20:34:10 -0700

    add basic http health check logic

---
+++ httpxtest.py
@@ -0,0 +1,26 @@
+import httpx
+import time
+
+def request_check(url):
+    try:
+        start = time.perf_counter()
+
+        request = httpx.get(url, timeout=5.0)
+
+        end = time.perf_counter()
+
+        time_taken = end - start
+        time_taken = round(time_taken, 2)
+        status = {"status code": request.status_code, "response time": time_taken, "Up": True, "Error": None}
+        return status
+    except httpx.RequestError:
+        end = time.perf_counter()
+        time_taken = end - start
+        time_taken = round(time_taken, 2)
+        return {"status code": None, "response time": time_taken, "Up": False, "Error":"Request Error"}
+
+
+test1 = request_check("https://manrajsingh.dev")
+print(test1)
+test2 = request_check("https://www.google.com/404")
+print(test2)
