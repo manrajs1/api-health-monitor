714edff475baa9ab7c0825e81f95131cfca6f7cf
Manraj Singh <162640559+manrajs1@users.noreply.github.com>
2026-08-27 00:37:59 +0000

    Add function for monitor and check history

--- database_test.py
+++ database_test.py
@@ -7,36 +7,8 @@ url = "https://example.com"
 
 monitor = add_monitor(url)
 
-<<<<<<< HEAD
-#Printing all monitors created so far
-res = cur.execute("SELECT id, url FROM monitors")
-print(res.fetchall())
-
-check_result = request_check(url)
-status_code = check_result["status code"]
-response_time = check_result["response time"]
-time = datetime.datetime.now(datetime.timezone.utc )
-timestamp = str(time)
-up = check_result["Up"]
-error = check_result["Error"]
-
-# Inserting into checks table data with linked monitor_id
-cur.execute("""
-INSERT INTO checks(monitor_id, status_code, response_time, 
-    timestamp, up, error)
-    VALUES(?,?,?,?,?,?)""",
-    (monitor_id, status_code, response_time, timestamp, up, error)
-)
-connection.commit()
-res = cur.execute("SELECT * FROM checks WHERE monitor_id = ? ", (monitor_id,))
-print(res.fetchall())
-
-
-connection.close()
-=======
 result = request_check(url)
 save_check(monitor["id"], result)
 
 history = get_check_history(monitor['id'])
 print(history)
->>>>>>> a53affe (add reusable database functions for monitors and check history)
